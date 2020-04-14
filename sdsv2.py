edee5ea66f439d0b89b28fbc6e9ffb5e9490164d
Mark Graham <50046031+markgraham924@users.noreply.github.com>
2020-04-14 23:35:03 +0100

    f

--- sdsv2.py
+++ sdsv2.py
@@ -5,12 +5,13 @@ import psutil
 from sds011 import *
 import aqi
 
-sensor = SDS011("COM3", use_query_mode=True)
-sensor.sleep(sleep=False)
+sensor = SDS011("COM3")
 
 def get_data(n=3):
+        sensor.sleep(sleep=False)
         pmt_2_5 = 0
         pmt_10 = 0
+        time.sleep(10)
         for i in range (n):
             x = sensor.query()
             pmt_2_5 = pmt_2_5 + x[0]
@@ -18,6 +19,7 @@ def get_data(n=3):
             time.sleep(2)
         pmt_2_5 = round(pmt_2_5/n, 1)
         pmt_10 = round(pmt_10/n, 1)
+        sensor.sleep(sleep=True)
         time.sleep(2)
         return pmt_2_5, pmt_10
     
@@ -51,7 +53,7 @@ while True:
     try:
         publish.single(topic, payload=tPayload, hostname=mqttHost, port=tPort, tls=tTLS, transport=tTransport)
         save_log()
-        time.sleep(12)
+        time.sleep(60)
     except:
         print ("[INFO] Failure in sending data")
         time.sleep(12)
